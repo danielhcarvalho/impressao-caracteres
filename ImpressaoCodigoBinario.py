4cf6691f1554015a77a99a82b8bff71c5dfa25bd
Daniel Henrique de Carvalho <168748383+danielhcarvalho@users.noreply.github.com>
2024-05-21 13:38:28 +0000

    Teste ImpressaoCodigoBinario.py

--- ImpressaoCodigoBinario.py
+++ ImpressaoCodigoBinario.py
@@ -1,6 +1,6 @@
 def imprimir_binario():
-    for i in range(1, 6):
-        for j in range(1, 6):
+    for i in range(0, 10):
+        for j in range(0, 10):
             if i % 2 == 0 and j % 2 == 0 or i % 2 != 0 and j % 2 != 0:
                 print('0 ', end='')
             else:
